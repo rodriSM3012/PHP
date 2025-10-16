5e87b06ba0627f8288ec6f464dee5b6b95032802
Rodrigo Sánchez <30.rodrigo.12@gmail.com>
2025-10-16 10:12:25 +0200

    add getters and setters

--- TEMA 2/ejercicio-tema2/clases/Miembros.php
+++ TEMA 2/ejercicio-tema2/clases/Miembros.php
@@ -15,8 +15,63 @@ abstract class Miembro
         $this->email = $email;
     }
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param $apellidos
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
     public function __toString()
     {
-        return $this->id ." " .$this->nombre ." ". $this->apellidos ." ". $this->email;
+        return $this->id . " " . $this->nombre . " " . $this->apellidos . " " . $this->email;
     }
 }
