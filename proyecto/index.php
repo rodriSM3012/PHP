9a64159556e215f0329087e620a347883c00c54f
Rodrigo Sánchez <30.rodrigo.12@gmail.com>
2025-10-23 11:50:40 +0200

    added index.php

---
+++ proyecto/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: monospace;
+        }
+    </style>
+</head>
+
+<body>
+    <main>
+        <h1>Menu de la tarea</h1>
+        <nav>
+            <ul>
+                <li><a href="./academy.php"></a></li>
+                <li><a href="./biblioteca.php"></a></li>
+                <li><a href="./triangle.php"></a></li>
+            </ul>
+        </nav>
+    </main>
+</body>
+
+</html>
