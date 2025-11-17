05e54fb4d2ea21ed06bc3955e40b1fc34038bd34
Rodrigo Sánchez <30.rodrigo.12@gmail.com>
2025-11-17 10:24:38 +0100

    add system to hide an option from the menu depending on the user's role

--- ejercicio-pizzeria/menu.php
+++ ejercicio-pizzeria/menu.php
@@ -3,6 +3,8 @@
         <li><a href="./index.php">Inicio</a></li>
         <li><a href="./login.php">Iniciar sesión</a></li>
         <li><a href="./logout.php">Cerrar sesión</a></li>
-        <li><a href="./admin-pizzas.php">Administrador</a></li>
+        <?php if (isset($_SESSION["rol"]) && $_SESSION["rol"] == "admin") { ?>
+            <li><a href="./admin-pizzas.php">Administrador</a></li>
+        <?php } ?>
     </ul>
 </nav>
