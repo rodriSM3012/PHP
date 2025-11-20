c8ab9b31f46da3cd9c02e0b221ab1d01bc9f2db9
Rodrigo Sánchez <30.rodrigo.12@gmail.com>
2025-11-20 12:15:16 +0100

    update index.php

--- pizzeria-putanesca/index.php
+++ pizzeria-putanesca/index.php
@@ -1,3 +1,19 @@
-<?php 
+<?php
 
 ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzeria Putanesca</title>
+</head>
+
+<body>
+    <?php
+    require_once("./views/partials/menu.php");
+    ?>
+</body>
+
+</html>
