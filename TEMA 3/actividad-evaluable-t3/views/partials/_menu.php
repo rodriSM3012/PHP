5d5097e62c19d620f7ce5b19b4e16d4b8c44d77e
rodrism3012 <30.rodrigo.12@gmail.com>
2025-11-27 15:17:47 +0100

    update menu

--- TEMA 3/actividad-evaluable-t3/views/partials/_menu.php
+++ TEMA 3/actividad-evaluable-t3/views/partials/_menu.php
@@ -1,7 +1,23 @@
 <nav>
+    <h2>Personajes</h2>
     <ul>
-        <li><a href="../characters/create_character.php">Personajes</a></li>
-        <li><a href="../enemies/create_enemy.php">Enemigos</a></li>
-        <li><a href="../items/create_item.php">Items</a></li>
+        <li><a href="../characters/create_character.php">Crear personaje</a></li>
+        <li><a href="../enemies/create_enemy.php">Lista de personajes</a></li>
+        <li><a href="../enemies/create_enemy.php">Editar personaje</a></li>
+        <li><a href="../items/create_item.php">Borrar personaje</a></li>
+    </ul>
+    <h2>Enemigos</h2>
+    <ul>
+        <li><a href="../characters/create_character.php">Crear enemigo</a></li>
+        <li><a href="../enemies/create_enemy.php">Lista de enemigoss</a></li>
+        <li><a href="../enemies/create_enemy.php">Editar enemigo</a></li>
+        <li><a href="../items/create_item.php">Borrar enemigo</a></li>
+    </ul>
+    <h2>Objetos</h2>
+    <ul>
+        <li><a href="../characters/create_character.php">Crear objeto</a></li>
+        <li><a href="../enemies/create_enemy.php">Lista de objetos</a></li>
+        <li><a href="../enemies/create_enemy.php">Editar objeto</a></li>
+        <li><a href="../items/create_item.php">Borrar objeto</a></li>
     </ul>
 </nav>
